753fdcc8b4d3483a54686605543e7dac03c90e07
Your Name <ngockhanh.2197@gmail.com>
2019-05-15 10:13:37 +0700

    clean code

--- js/index.js
+++ js/index.js
@@ -1,38 +1,18 @@
 $(document).ready(function () {
-	pressBtn('#shaker');   	
-	output ="&#x2685; "+"&#x2685; ";
-	document.getElementById('dice').innerHTML = output;
-	$('#shaker').on('click', function() {
-		$('#text').text('Shake, shake, shake...');
-		$('#text').addClass('shake');
-		setTimeout(function(){
-			rollTheDice();
-			$('#text').removeClass('shake');
-		}, 200);
-	});	
-	function rollTheDice()
-	{
-		var i,
-		number=0,
-		faceValue,
-		output = '';
-		diceCount = document.querySelector('input[type=number]').value || 2;
-        for (i = 0; i < diceCount; i++) {
-        	faceValue = Math.floor(Math.random() * 6);
-        	number+=(faceValue+1);
-        	output += "&#x268" + faceValue + "; ";
-        }
-        document.getElementById('dice').innerHTML = output;
-        if ((number)==diceCount)
-        {
-        	document.getElementById('text').innerHTML = "Snake eyes!";
-        } 
-        else {
-        	document.getElementById('text').innerHTML = number;
-        }
-        
-    }
-	function pressBtn(button) {
+	//elements
+	const eleTextNumber = document.querySelector('input[type=number]');
+	const eleDice = document.getElementById('dice');
+	const eleText =document.getElementById('text');
+
+	//variables
+	const delayTime = 300;
+	var output;
+	const point = "&#x268" ;
+	const textSnake = "Snake eyes!";
+	const textShake = 'Shake, shake, shake...';
+
+	//function press button Shake
+	const pressBtn =(button) => {
 		$('#shaker').on('mousedown', function() {
 			$(this).removeClass('btn_up');
 			$(this).addClass('btn_down');
@@ -42,4 +22,48 @@ $(document).ready(function () {
 			$(this).addClass('btn_up');
 		});
 	}
+
+	//function callback when click
+	const clickButton = () => {
+		$('#text').text(textShake);
+		$('#text').addClass('shake');
+		setTimeout(function(){
+			rollTheDice();
+			$('#text').removeClass('shake');
+		}, delayTime);
+	}
+
+	//function xu li khi nhan nut shake
+	const rollTheDice = () => {
+		let i,
+		number=0,
+		faceValue,
+		output = '';
+		var diceCount = eleTextNumber.value || 2;
+		for (i = 0; i < diceCount; i++) {
+			faceValue = Math.floor(Math.random() * 6);
+			number+=(faceValue+1);
+			output += point + faceValue + "; ";
+		}
+		eleDice.innerHTML = output;
+		if ((number)==diceCount)
+		{
+			eleText.innerHTML = textSnake;
+		} 
+		else {
+			eleText.innerHTML = number;
+		}
+
+	}
+	//Function init
+	const init = () => {
+		pressBtn('#shaker');   	
+		output ="&#x2685; "+"&#x2685; ";
+		eleDice.innerHTML = output;
+	}
+	
+	init();
+	$('#shaker').on('click', function(){
+		clickButton();
+	});
 });
