291424c16afbade0d73250d4878e2d532487806b
Your Name <ngockhanh.2197@gmail.com>
2019-05-17 14:35:55 +0700

    add gitignore

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,6 @@
 var express = require('express')
 var router = express.Router()
 
-// middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
 router.get("/", function(request, response)  {
     
     response.render("index");
