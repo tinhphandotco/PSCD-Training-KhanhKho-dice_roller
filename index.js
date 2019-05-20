0fa8a4436c2fd735d7543afa5f8c758dd5affd86
Your Name <ngockhanh.2197@gmail.com>
2019-05-20 15:15:02 +0700

    create login

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+require('dotenv').config()
 const express = require("express");
 const router = require('./routes/router');
+const morgan = require("morgan")
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public')); 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(morgan(':date[web] :method :url'))
+// app.use(morgan());
+app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use('/', router);
+
+app.post('/auth', function (request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        if (username === 'user1' && password === "123") {
+            request.session.loggedin = true;
+            request.session.username = username;
+            response.redirect('/');
+        } else {
+            if (username != 'user1' && password != '123')
+                response.send('Incorrect Username and Password !');
+            if (username != 'user1')
+                response.send('Incorrect Username !');
+            if (password != '123')
+                response.send('Incorrect password !');
+        }
+        response.end();
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
 
 // app.listen(3000);
 app.listen(port, () => console.log(`app listening on port ${port}!`))
--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
+
 router.get("/", function(request, response)  {
     
     response.render("index");
+});
+router.get("/login", function(request, response)  {
+    
+    response.render("login");
 });
 
 module.exports = router
