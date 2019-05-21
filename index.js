c51d5f2de46af677907ef6ebcbbc98357a33a6a1
Your Name <ngockhanh.2197@gmail.com>
2019-05-21 10:52:04 +0700

    function logout

--- index.js
+++ index.js
@@ -16,35 +16,11 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(morgan(':date[web] :method :url'))
-// app.use(morgan());
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(morgan(':date[web] :method :status :url'));
 app.use('/', router);
-
-app.post('/auth', function (request, response) {
-    var username = request.body.username;
-    var password = request.body.password;
-    if (username && password) {
-        if (username === 'user1' && password === "123") {
-            request.session.loggedin = true;
-            request.session.username = username;
-            response.redirect('/');
-        } else {
-            if (username != 'user1' && password != '123')
-                response.send('Incorrect Username and Password !');
-            if (username != 'user1')
-                response.send('Incorrect Username !');
-            if (password != '123')
-                response.send('Incorrect password !');
-        }
-        response.end();
-    } else {
-        response.send('Please enter Username and Password!');
-        response.end();
-    }
-});
 
 // app.listen(3000);
 app.listen(port, () => console.log(`app listening on port ${port}!`))
--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,78 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
 
-
-router.get("/", function(request, response)  {
-    
-    response.render("index");
+router.get("/login", function (request, response) {
+    response.render("login");
 });
-router.get("/login", function(request, response)  {
-    
+router.post('/login', function (request, response) {
+    let username = request.body.username;
+    let password = request.body.password;
+    if (username && password) {
+        if (username === 'user1' && password === "123") {
+
+            request.session.userAuth= {
+                username:username,
+                password:password,
+            }
+            request.session.loggedin = true;
+            response.redirect('/');
+        } 
+        else {
+            if (username != 'user1' && password != '123') {
+                response.render("login", {
+                    messageuser: "Incorrect Username !",
+                    messagepass: "Incorrect Password !"
+                });
+            }
+            if (username == '')
+                response.render("login", {
+                    messageuser: "Username is not empty !"
+                })
+            else {
+                if (username != 'user1') {
+                    if (password == '123') tmppass = password;
+                    response.render("login", {
+                        messageuser: "Incorrect Username !",
+                        tmppass
+                    });
+                }
+
+            }
+
+            if (password == '')
+                response.render("login", {
+                    messagepass: "Password is not empty !"
+                })
+            else {
+                if (password != '123') {
+                    if (username == 'user1') tmpuser = username;
+                    response.render("login", {
+                        messagepass: "Incorrect Password !",
+                        tmpuser
+                    });
+                }
+
+            }
+        }
+    }
+    // else {
+    //     response.send('Please enter Username and Password!');
+    //     response.end();
+    // }
+});
+router.get('/', function (request, response) {
+    if (request.session.loggedin) {
+        response.render("index", {
+            username: request.session.userAuth.username
+        });
+    } else {
+        response.render("login");
+    }
+    response.end();
+});
+
+router.get('/logout', function (request, response) {
+    request.session.destroy();
     response.render("login");
 });
 
