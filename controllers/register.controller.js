193a8727df06c2d4bdee4cb90cfa8e5085527e08
Your Name <ngockhanh.2197@gmail.com>
2019-05-23 21:30:42 +0700

    fix bug

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -15,26 +15,51 @@ const register = async (request, response) => {
         password,
         re_password,
     } = request.body;
+    if (firstname && lastname && username && email && password && re_password) {
+        let user = await User.findOne({ username }).exec();
+        if (user) {
+            response.render("register", {
+                messageuser: "Username already exist"
+            });
+        }
+        let e_mail = await User.findOne({ email }).exec();
+        if (e_mail) {
+            response.render("register", {
+                messageemail: "Email already exist"
+            });
+        }
 
-    if (password != re_password) {
-        response.render("register", {
-            message: "Confirm password is incorrect"
-        });
+        if (password != re_password) {
+            response.render("register", {
+                message: "Confirm password is incorrect"
+            });
+        }
+        else {
+
+            const hashedPassword = bcrypt.hashSync(password, 10);
+            const user = new User({
+                firstname,
+                lastname,
+                username,
+                email,
+                password: hashedPassword,
+            });
+
+            await user.save();
+            return response.redirect("/login");
+        }
     }
     else {
-
-        const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = new User({
-            firstname,
-            lastname,
-            username,
-            email,
-            password: hashedPassword,
-        });
-
-        await user.save();
-        return response.redirect("/login");
+        response.render("register", {
+            messagefirst: firstname == '' ? "Firstname is not empty !" : '',
+            messagelast: lastname == '' ? "Lastname is not empty !" : '',
+            messageuser: username == '' ? "Username is not empty !" : '',
+            messageemail: email == '' ? "Email is not empty !" : '',
+            messagepass: password == '' ? "Password is not empty !" : '',
+            message: re_password == '' ? "Re_password is not empty !" : '',
+        })
     }
+
 };
 
 module.exports = {
